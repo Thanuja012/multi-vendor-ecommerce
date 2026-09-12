5398221bcd4a45618f8450d849259e2b068d636c
Thanu <thanu@gmail.com>
2026-09-12 19:20:56 +0530

    connect frontend to deployed backend

--- backend/src/main/java/com/ecommerce/config/SecurityConfig.java
+++ backend/src/main/java/com/ecommerce/config/SecurityConfig.java
@@ -76,7 +76,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+       config.setAllowedOrigins(List.of(
+    "http://localhost:3000",
+    "https://multi-vendor-ecommerce-liard.vercel.app"
+));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
